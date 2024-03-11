4fba89a6071463f4f48d68b4aa062cf2578ffe5d
Pierre Blarre <pierre.blarre@gmail.com>
2024-03-11 16:31:56 +0100

    Moving to PDO instead of deprecated mysqli driver

--- fakerMySQLI.php
+++ fakerMySQLI.php
@@ -6,31 +6,26 @@
 require_once 'vendor/autoload.php';
 
 // Connect to the database
-$user = 'phpmy';
-$password = 'phpmy';
-$dbname = 'blogbdd';
-$driver = 'mysql';
+$conn = new mysqli("localhost", "phpmy", "phpmy", "blog");
 
-try {
-    $conn = new PDO($driver.':host=localhost;dbname='.$dbname, $user, $password);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-
 
 // Generate and insert the fake data for the USERS table
 $faker = Faker\Factory::create();
 for ($i = 0; $i < 5; $i++) {
     $user_email = $faker->email();
-    $stmt = $conn->prepare("INSERT INTO USERS (email) VALUES (:email)");
-    $stmt->bindParam(":email", $user_email, PDO::PARAM_STR);
+    $stmt = $conn->prepare("INSERT INTO USERS (email) VALUES (?)");
+    $stmt->bind_param('s', $user_email); // 's' specifies the variable type => 'string'
     $stmt->execute();
 }
-echo "USERS inserted successfully"."\n";
+echo "Users inserted successfully"."\n";
 
 //Fetch USERS ids
 $result = $conn->query("SELECT id FROM USERS");
-$users_ids = $result->fetchAll(PDO::FETCH_ASSOC);
+$users_ids = $result->fetch_all(MYSQLI_ASSOC);
 
 // Generate and insert the fake data for the ARTICLES table
 for ($i = 0; $i < 50; $i++) {
@@ -39,18 +34,15 @@ for ($i = 0; $i < 50; $i++) {
     $date = $faker->date();
     $user_id = $users_ids[array_rand($users_ids)]['id']; //pick a random user id
 
-    $stmt = $conn->prepare("INSERT INTO ARTICLES (title, content, date_created, USERS_id) VALUES  (:title, :content, :date_created, :USERS_id)");
-    $stmt->bindParam(':title', $title, PDO::PARAM_STR);
-    $stmt->bindParam(':content', $content, PDO::PARAM_STR);
-    $stmt->bindParam(':date_created', $date, PDO::PARAM_STR);
-    $stmt->bindParam(':USERS_id', $user_id, PDO::PARAM_INT);
+    $stmt = $conn->prepare("INSERT INTO ARTICLES (title, content, date_created, USERS_id) VALUES  (?, ?, ?, ?)");
+    $stmt->bind_param('sssi', $title, $content, $date, $user_id); // 's' specifies the variable type => 'string', 'i' => integer
     $stmt->execute();
 }
 echo "Articles inserted successfully"."\n";
 
 //Fetch ARTICLES ids
 $result = $conn->query("SELECT id FROM ARTICLES");
-$articles_ids = $result->fetchAll(PDO::FETCH_ASSOC);
+$articles_ids = $result->fetch_all(MYSQLI_ASSOC);
 
 // Generate and insert the fake data for the COMMENTS table
 for ($i = 0; $i < 100; $i++) {
@@ -58,10 +50,8 @@ for ($i = 0; $i < 100; $i++) {
     $user_id = $users_ids[array_rand($users_ids)]['id']; //pick a random user id
     $content = $faker->realText();
 
-    $stmt = $conn->prepare("INSERT INTO COMMENTS (ARTICLES_id, USERS_id, content) VALUES  (:ARTICLES_id, :USERS_id, :content)");
-    $stmt->bindParam(':ARTICLES_id', $article_id, PDO::PARAM_INT);
-    $stmt->bindParam(':USERS_id', $user_id, PDO::PARAM_INT);
-    $stmt->bindParam(':content', $content, PDO::PARAM_STR);
+    $stmt = $conn->prepare("INSERT INTO COMMENTS (ARTICLES_id, USERS_id, content) VALUES  (?, ?, ?)");
+    $stmt->bind_param('iis', $article_id, $user_id, $content); // 's' specifies the variable type => 'string', 'i' => integer
     $stmt->execute();
 }
 echo "COMMENTS inserted successfully"."\n";
@@ -78,9 +68,8 @@ foreach ($users_ids as $user) {
     }
     $already_inserted[] = $user_id1."-".$user_id2;
 
-    $stmt = $conn->prepare("INSERT INTO USERS_USERS (USERS_id1, USERS_id2) VALUES  (:USERS_id1, :USERS_id2)");
-    $stmt->bindParam(':USERS_id1', $user_id1, PDO::PARAM_INT);
-    $stmt->bindParam(':USERS_id2', $user_id2, PDO::PARAM_INT);
+    $stmt = $conn->prepare("INSERT INTO USERS_USERS (USERS_id1, USERS_id2) VALUES  (?, ?)");
+    $stmt->bind_param('ii', $user_id1, $user_id2); // 'i' => integer
     $stmt->execute();
 }
 echo "USERS_USERS inserted successfully"."\n";
@@ -88,6 +77,6 @@ echo "USERS_USERS inserted successfully"."\n";
 echo "Fake data inserted successfully"."\n";
 
 // Close the connection
-;
+$conn->close();
 
 
